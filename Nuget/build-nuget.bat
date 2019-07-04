c8d151f689ff778512e952c94a4c85d16b664ffc
Aleksey Timofeev <atimofeev@oneincsystems.com>
2019-07-04 12:32:10 +0500

    fixed up with new version of nuget, push to nuget.org version 1.0.1.4

--- Nuget/build-nuget.bat
+++ Nuget/build-nuget.bat
@@ -6,7 +6,7 @@ if not [%1]==[] set path=%1
 IF NOT EXIST Packages MKDIR Packages
 
 echo Creating %packageName% Package
- %path%\..\.nuget\nuget pack %path%\..\%packageName%.csproj -IncludeReferencedProjects -OutputDirectory %path%\..\Nuget\Packages -Properties Configuration=Release -Verbose 
+ %path%\..\.nuget\nuget pack %path%\..\%packageName%.csproj -IncludeReferencedProjects -OutputDirectory %path%\..\Nuget\Packages -Properties Configuration=Release
 
 if errorlevel 1 echo Error creating %packageName% Package
 

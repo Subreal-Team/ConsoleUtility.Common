7ae030cc326a89fc568d99993322b92299b2c5f5
AlexSnowLeo <alex.v.timofeev@gmail.com>
2019-07-23 14:11:03 +0500

    release 1.0.2.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.2.1")]
 [assembly: AssemblyFileVersion("1.0.2.1")]
-[assembly: AssemblyInformationalVersion("1.0.2.1-RC3")]
+[assembly: AssemblyInformationalVersion("1.0.2.1")]
